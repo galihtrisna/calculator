78c517446ce4baebca916367dac4bb747095541d
Galih Trisna <galihtrisna666@gmail.com>
2023-03-14 08:32:18 +0700

    fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ total_expression = ""
 current_expression = "0"
 
 root = tk.Tk()
-root.geometry("375x667") #ukuran window
+root.geometry("375x800") #ukuran window
 root.resizable(0, 0) # agar tidak bisa di maximaze
 root.title("Calculator") #judul aplikasi
 root.configure(background="black")
@@ -13,10 +13,10 @@ root.configure(background="black")
 frame = tk.Frame(root, height=210, bg="#F5F5F5") 
 frame.pack(expand=True, fill="both")
 
-total_label= tk.Label(frame, text=total_expression, anchor=tk.E, bg="#F4F4F4", fg="#25265E", padx=24, font=("Arial", 16), wraplength=375, justify="right")
+total_label= tk.Label(frame, text=total_expression, anchor=tk.E, bg="#22252D", fg="#FFFFFF", padx=24, font=("Poppins", 16), wraplength=375, justify="right")
 total_label.pack(expand=True, fill='both')
 
-label = tk.Label(frame, text=current_expression, anchor=tk.E, bg="#F5F5F5", fg="#25265E", padx=24, font=("Arial", 40, "bold"), wraplength=375, justify="right")
+label = tk.Label(frame, text=current_expression, anchor=tk.E, bg="#22252D", fg="#FFFFFF", padx=24, font=("Poppins", 40, "bold"), wraplength=375, justify="right")
 label.pack(expand=True, fill='both')
 
 
@@ -102,28 +102,28 @@ for x in range(1, 5):
 
 
 for digit, grid_value in digits.items():
-    button = tk.Button(btn_frame, text=str(digit), bg="#ffffff", fg="#25265E", font=("Arial", 24,"bold"),borderwidth=0, command=lambda x=digit: add_digit(x))
+    button = tk.Button(btn_frame, text=str(digit), bg="#292D36", fg="#ffffff", font=("Poppins", 24,"bold"),borderwidth=0, command=lambda x=digit: add_digit(x))
     button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
 
 
 i = 0
 for operator, symbol in operations.items():
-    button = tk.Button(btn_frame, text=symbol, bg="#F8FAFF", fg="#25265E", font=("Arial", 20),
+    button = tk.Button(btn_frame, text=symbol, bg="#292D36", fg="#EB6666", font=("Poppins", 20),
                         borderwidth=0, command=lambda x=operator: add_operator(x))
     button.grid(row=i, column=4, sticky=tk.NSEW)
     i += 1
 
 
-button_clear = tk.Button(btn_frame, text="C", bg="#F8FAFF", fg="#25265E", font=("Arial", 20),
+button_clear = tk.Button(btn_frame, text="C", bg="#292D36", fg="#26F4CE", font=("Poppins", 20),
                            borderwidth=0, command=clear)
 button_clear.grid(row=0, column=1, sticky=tk.NSEW)
-button_equal = tk.Button(btn_frame, text="=", bg="orange", fg="#25265E", font=("Arial", 20),
+button_equal = tk.Button(btn_frame, text="=", bg="#ffffff", fg="#292D36", font=("Poppins", 20),
                            borderwidth=0, command=evaluate)
 button_equal.grid(row=4, column=3, columnspan=2, sticky=tk.NSEW)
-button_square = tk.Button(btn_frame, text="x\u00b2", bg="#F8FAFF", fg="#25265E", font=("Arial", 20),
+button_square = tk.Button(btn_frame, text="x\u00b2", bg="#292D36", fg="#26F4CE", font=("Poppins", 20),
                            borderwidth=0, command=square)
 button_square.grid(row=0, column=2, sticky=tk.NSEW)
-button_delete = tk.Button(btn_frame, text="\u232b", bg="#F8FAFF", fg="#25265E", font=("Arial", 20),borderwidth=0, command=delete)
+button_delete = tk.Button(btn_frame, text="\u232b", bg="#292D36", fg="#26F4CE", font=("Poppins", 20),borderwidth=0, command=delete)
 button_delete.grid(row=0, column=3, sticky=tk.NSEW)
 
 # bind return atinya untuk menghadle program ketika mengetik enter
